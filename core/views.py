f9f939a95f81bce69fb1763c9f1dfcafb5b4a4f3
Gustavo Veloso <gjmveloso@gmail.com>
2011-02-07 22:08:16 -0200

    1º sprint

---
+++ core/views.py
@@ -0,0 +1,12 @@
+# Create your views here.
+
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+def homepage(request):
+    context = RequestContext(request)   
+    return render_to_response('homepage.html', context)
+
+
+
---
+++ urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls.defaults import *
+from core.views import *
+from django.conf import settings
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('', 
+    (r'^$', homepage),
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve', { 'document_root': settings.MEDIA_ROOT }) 
+    # Example:
+    # (r'^eventex/', include('eventex.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # (r'^admin/', include(admin.site.urls)),
+)
