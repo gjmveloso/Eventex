cfb233c17ba3f15ae9c06dfc96c08fc8fdab9f10
Gustavo Veloso <gjmveloso@gmail.com>
2011-02-20 20:56:54 -0300

    Instalando south

--- settings.py
+++ settings.py
@@ -108,6 +108,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'core',
     'subscription',
+    'south',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
