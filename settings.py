8c60b2d4855a554f6eaa28d835da9846fdeb0e4d
Gustavo Veloso <gjmveloso@gmail.com>
2011-02-20 20:49:25 -0300

    Sem South

--- settings.py
+++ settings.py
@@ -108,7 +108,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'core',
     'subscription',
-    'south',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
